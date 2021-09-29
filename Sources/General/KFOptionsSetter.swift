93b6773544aa6e2a6a6d696d26466340a5d10494
dung.vu <dungvh@galaxy.com.vn>
2021-09-29 14:39:08 +0700

    [ App ] **Update**  : Update code

--- Sources/General/KFOptionsSetter.swift
+++ Sources/General/KFOptionsSetter.swift
@@ -41,7 +41,7 @@ extension KF.Builder: KFOptionSetter {
     public var delegateObserver: AnyObject { self }
 }
 
-#if canImport(SwiftUI) && canImport(Combine)
+#if canImport(SwiftUI) && canImport(Combine) && !(os(iOS) && (arch(i386) || arch(arm)))
 @available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
 extension KFImage: KFOptionSetter {
     public var options: KingfisherParsedOptionsInfo {
